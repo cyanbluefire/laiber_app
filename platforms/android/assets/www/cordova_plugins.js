5f83477c2fdc6cca95e43b655c25720ea5c78cfb
cyan2 <313218599@qq.com>
2015-06-12 17:57:11 +0800

    getbulm

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -39,6 +39,20 @@ module.exports = [
         "clobbers": [
             "toast"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -47,7 +61,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.0.0",
     "org.apache.cordova.console": "0.2.13",
     "org.apache.cordova.camera": "0.3.6",
-    "com.chariotsolutions.toast.plugin": "1.1.1"
+    "com.chariotsolutions.toast.plugin": "1.1.1",
+    "org.apache.cordova.dialogs": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
